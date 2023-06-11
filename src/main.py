c0a5e98409a03762aa1b4799ff33891ec5432004
Sebastian Sangervasi <sebastian@maxretail.com>
2023-06-11 12:56:50 -0700

    fix ipython version

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
+import dataclasses
+import itertools
+import os
+import pathlib
+
+
 def main():
     print("Blank")
+    return 'Blank'
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,13 @@
+from main import main
+
+def test_main():
+    assert main() == 'Blank'
+
+
 def test_fail():
     breakpoint()
     raise Exception('fail')
+
 
 
 def test_breakpoint():
